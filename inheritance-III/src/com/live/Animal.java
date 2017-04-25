c31daa01968419a487aba4fd85d0b1e58362f74f
nag <nagabhushanamn@gmail.com>
2017-04-25 10:01:44 +0530

    oops

---
+++ inheritance-III/src/com/live/Animal.java
@@ -0,0 +1,9 @@
+package com.live;
+
+public class Animal extends LivingThing {
+
+	@Override
+	public void work() {
+		System.out.println("Animal Work...");
+	}
+}
---
+++ inheritance-III/src/com/live/LivingThing.java
@@ -0,0 +1,19 @@
+package com.live;
+
+public abstract class LivingThing {
+
+	// common prop
+
+	// common behav
+
+	public void eat() {
+		System.out.println("LT eat");
+	}
+
+	public void sleep() {
+		System.out.println("LT sleep");
+	}
+
+	public abstract void work();
+
+}
